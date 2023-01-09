386e7e50d485a22eacb9acfb631072ed42e119ab
SalvaMrS <salvadorsanchez1832005@gmail.com>
2023-01-09 13:14:18 -0300

    Mejora de Ordenar.sh

--- Ordenar.sh
+++ Ordenar.sh
@@ -1,46 +1,59 @@
 #!/bin/bash
 
-# Falta mover carpetas y extensiones extrañas (Otros)
-# y agrupar por usuarios y fechas
-# Cambiar nombre de la configuración y permitir más configuraciones
-
+# ubicación del directorio origen
 ubicacion="/home/Salva/Ordenar"
+
+# ubicación del directorio destino
 destino="/home/Salva/Nuevo"
+
+# archivo de extensiones
 archivo="Extensiones.txt"
 
-
+# función para mover los archivos según la extensión especificada a la carpeta correspondiente en el directorio del usuario
 mover () {
+  # busca los archivos con la extensión especificada en el directorio origen y los guarda en un archivo temporal
+  ls -1 "$ubicacion"/ | grep ".*$1$" > temp.txt
 
-ls -1 $ubicacion/ | grep ".*$1$" > temp.txt
+  # itera sobre cada línea del archivo temporal
+  while read -r linea; do
+    # verifica si la carpeta para el usuario en el directorio destino ya existe
+    usuario=$(stat -c "%U" "$ubicacion/$linea")
 
-while IFS= read -r linea || [[ -n "$linea" ]]; do 
+    mkdir -p "$destino/$usuario"
+    
+    mkdir -p "$destino/$usuario/$2"
 
-	if [[ ! -d $destino/$2 ]]; then
-		mkdir $destino/$2
-	fi
-	
-	mv $ubicacion/"$linea" $destino/$2/
+    # mueve el archivo al directorio del usuario en el directorio destino
+    mv "$ubicacion/$linea" "$destino/$usuario/$2/"
 
-done < temp.txt
+  done < temp.txt
 }
 
-while IFS= read -r li || [[ -n "$li" ]]; do 
+# itera sobre cada línea del archivo de extensiones
+while read -r li; do
+  # contador para recorrer los elementos de la línea
+  contador=0
+  # nombre de la carpeta para el tipo de archivo
+  carpeta=""
 
-	contador=0
-	carpeta=""
+  # recorre cada elemento de la línea
+  for i in $li; do
+    # si es el primer elemento de la línea, se trata del nombre de la carpeta para el tipo de archivo
+    if [ $contador == 0 ]; then
+      carpeta=$i
+    # si no es el primer elemento, se trata de una extensión y se mueven los archivos correspondientes
+    else
+      mover "\.$i" "$carpeta"
+    fi
 
-	for i in $li; do
-		if [ $contador == 0 ]; then
-			carpeta=$i
-		else
-			mover "\.$i" $carpeta
-		fi
+    # aumenta el contador
+    let contador++
+  done
+done < "$archivo"
 
-		let contador++
-
-	done
-done < $archivo
-
+# mueve los archivos con extensiones no especificadas en el archivo de extensiones a la carpeta "otros"
 mover "\..*" "otros"
 
+# mueve los directorios a la carpeta "carpetas"
 mover '' "carpetas"
+
